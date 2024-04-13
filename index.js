af9729f9cfb5f48f9f1fa984be7025eeb07bd7ff
Trevezan <b39s@hotmail.com>
2024-04-13 19:28:25 -0300

    feat: "routes for 'eventsRoutes' "

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userRoutes from "./routes/users.js";
+import eventsRoutes from "./routes/events.js";
 import cors from "cors";
 
 const app = express();
@@ -7,6 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/", eventsRoutes);
 app.use("/", userRoutes);
-
 app.listen(3030);
