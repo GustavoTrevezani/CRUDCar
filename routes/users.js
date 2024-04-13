7b7c4ae8ca2e092802d74150027a6f110838bb9f
Trevezan <b39s@hotmail.com>
2024-04-13 18:40:56 -0300

    feat: "Routes"

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,20 @@
 import { getUsers } from "../controllers/user.js";
+import { getUniqueUsers } from "../controllers/user.js";
+import { postUsers } from "../controllers/user.js";
+import { putUsers } from "../controllers/user.js";
+import { deleteUsers } from "../controllers/user.js";
 import { Router } from "express";
 
 const router = Router();
-//                 conecta ao user do controller
+//Rota de pesquisa Todos os usuarios
 router.get("/", getUsers);
+//Rota de pesquisa único usuário
+router.get("/:id", getUniqueUsers);
+//Rotas de criação de um usuário
+router.post("/", postUsers);
+//Rota de atualização de um usuário
+router.put("/:id", putUsers);
+//Rota de deletar um usuário
+router.delete("/:id", deleteUsers);
 
 export default router;
