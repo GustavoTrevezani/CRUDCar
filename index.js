876593b0e6afb37ddc823bbba4c66ca2d53410e4
Trevezan <b39s@hotmail.com>
2024-04-13 19:36:08 -0300

    feat: "cors removed"

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import express from "express";
 import userRoutes from "./routes/users.js";
 import eventsRoutes from "./routes/events.js";
-import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 app.use("/", eventsRoutes);
 app.use("/", userRoutes);
